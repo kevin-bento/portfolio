65be16ca81d45b5ba7602e74771ca366e531d3a2
Kevin Liu (21012758) <k55liu@uwaterloo.ca>
2025-01-22 18:11:20 -0500

    Updates

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export const Footer = () => {
-    return (
-        <footer class=" flex justify-start items-center h-32 sticky bottom-0 z-1">
-            <p class="text-white m-16">Kevin Liu 2024.</p>
-        </footer>
-  );
-};
-
